3138245c54fa4a8449dc9e685988b2815e8b0c99
Igor Zhuravkov <zhuravkovigor@outlook.com>
2020-11-03 14:46:49 +1000

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
-// for heroku
-app.use(express.static("client/build"));
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "index.html"));
-});
-
 app.use("/api/products", require("./routes/products.routes"));
 app.use("/api/media", require("./routes/media.routes"));
 app.use("/api/mail", require("./routes/mail.routes"));
+
+// for heroku
+app.use(express.static("client/build"));
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 async function connect() {
 	try {
