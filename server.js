9e3bbb5ee7c5e1f350ecfc1318632ca7685c8a05
Igor Zhuravkov <zhuravkovigor@outlook.com>
2020-11-03 14:09:58 +1000

    added a new port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // for heroku
